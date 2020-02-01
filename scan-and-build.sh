773a253e5387c25e20163042a6e5ceeaab520820
YK Liu <cos.lyk@gmail.com>
2020-02-01 11:08:35 +0100

    Do not break if one package fails to build

--- scan-and-build.sh
+++ scan-and-build.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+EXIT_CODE=0
+
 ls recipes | while read SUBDIR; do
-    ./build-deb.sh recipes/$SUBDIR || exit 1
+    ./build-deb.sh recipes/$SUBDIR
+    if [ $? != 0 ]; then
+        EXIT_CODE=1
+    fi
 done
+
+exit $EXIT_CODE
