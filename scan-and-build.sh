06fb6adf554acdfd86f953e7be1d7e05b9ce0675
YK Liu <cos.lyk@gmail.com>
2020-02-09 19:52:27 +0100

    Fix exit code

--- scan-and-build.sh
+++ scan-and-build.sh
@@ -3,10 +3,7 @@
 EXIT_CODE=0
 
 ls recipes | while read SUBDIR; do
-    ./build-deb.sh recipes/$SUBDIR
-    if [ $? != 0 ]; then
-        EXIT_CODE=1
-    fi
+    ./build-deb.sh recipes/$SUBDIR || EXIT_CODE=1
 done
 
 exit $EXIT_CODE
