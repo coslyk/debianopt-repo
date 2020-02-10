3c4cd52116cddac2b9da7cccff3e9b3d2d55c8b1
YK Liu <cos.lyk@gmail.com>
2020-02-10 19:35:46 +0100

    Change building rules for pull request

--- scan-and-build.sh
+++ scan-and-build.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-EXIT_CODE=0
 
-ls recipes | while read SUBDIR; do
-    ./build-deb.sh recipes/$SUBDIR || EXIT_CODE=1
-done
+if [ -z "$TRAVIS_PULL_REQUEST_BRANCH" ]; then    # Normal commit
+    EXIT_CODE=0
+    ls recipes | while read SUBDIR; do
+        ./build-deb.sh recipes/$SUBDIR || EXIT_CODE=1
+    done
+    exit $EXIT_CODE
 
-exit $EXIT_CODE
+else  # Pull request
+    ./build-deb.sh recipes/$TRAVIS_PULL_REQUEST_BRANCH
+fi
